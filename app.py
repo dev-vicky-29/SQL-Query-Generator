7fa8469eae51a5b8529fb6d6f400f5b4b4b3c631
dev-vicky-29 <150275960+dev-vicky-29@users.noreply.github.com>
2024-07-01 18:20:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def main():
     st.markdown(
         """
             <div style="text-align: center">
-            <h1>🛢️</>👨🏼‍💻 SQL Query Generator 🛢️</>👨🏼‍💻</h1>
+            <h1>🛢️👨🏼‍💻🛢 SQL Query Generator 🛢️👨🏼‍💻🛢</h1>
             <h3>I can Generate SQL Query for You!</h3>
             <h4>With Explanation as Well !!</h4>
             <p>This tool is a simple tool that allows you to Generate SQL Queries based on your Prompts.</p>

eb00855db0de88886f69360fec6554df6f8308ca
dev-vicky-29 <150275960+dev-vicky-29@users.noreply.github.com>
2024-08-20 17:25:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,10 +22,7 @@ def main():
             <p>This tool is a simple tool that allows you to Generate SQL Queries based on your Prompts.</p>
             
             <!-- Button to go to the updated version of Database Query Generator -->
-            
-            <form action="app2" method="get">
             <button style="background-color: #4CAF50; color: white; padding: 14px 20px; margin: 10px 0; border: none; cursor: pointer; width: 100%; text-align: center; font-size: 16px;"> Go to Updated Database Query Generator</button>
-        </form>
             </div>
         """,
         unsafe_allow_html=True,
